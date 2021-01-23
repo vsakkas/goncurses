e1f9054fd55f62cf399fc52cc041de31ff2d5a2c
vsakkas <vasileios.sakkas96@gmail.com>
2021-01-23 20:55:06 +0200

    Add control modifiers (#11)

--- defs.go
+++ defs.go
@@ -276,3 +276,33 @@ const (
 	M_SHIFT          = C.BUTTON_SHIFT          // shift-click
 	M_POSITION       = C.REPORT_MOUSE_POSITION // mouse moved
 )
+
+// Control modifiers
+const (
+	CTRL_A = 1
+	CTRL_B = 2
+	CTRL_C = 3
+	CTRL_D = 4
+	CTRL_E = 5
+	CTRL_F = 6
+	CTRL_G = 7
+	CTRL_H = 8
+	CTRL_I = 9
+	CTRL_J = 10
+	CTRL_K = 11
+	CTRL_L = 12
+	CTRL_M = 13
+	CTRL_N = 14
+	CTRL_O = 15
+	CTRL_P = 16
+	CTRL_Q = 17
+	CTRL_R = 18
+	CTRL_S = 19
+	CTRL_T = 20
+	CTRL_U = 21
+	CTRL_V = 22
+	CTRL_W = 23
+	CTRL_X = 24
+	CTRL_Y = 25
+	CTRL_Z = 26
+)
